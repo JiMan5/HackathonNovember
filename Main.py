63baed83280d69cbd176e60d4755c88c00ead114
Orestistsira <74668066+Orestistsira@users.noreply.github.com>
2020-11-20 21:41:30 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,40 +5,44 @@ Description = "From the outside this house looks magnificent. It has been built 
 
 
 Types = [
-    'programming',
-    'photos',
-    'painting',
-    'music',
-    'maths',
-    'literature',
-    'gaming',
-    'cooking',
-    'cine',
-    'boardgame',
-    'basket',
-    'tennis',
-    'theater'
+    'programming','photos','painting','music','maths','literature','gaming','cooking','cine',
+    'boardgame','basket','tennis','theater'
 ]
 
 Users = [
-    User("John", "John@gmail.com", "dscdsc", 20, "Toumpa", Types[4], 0, 0),
-    User("Helen", "Helen@gmail.com", "fdvdfv", 24, "Toumpa", Types[4], 0, 0),
-    User("John", "John@gmail.com", "dscdsc", 20, "Toumpa", Types[4], 0, 0),
-    User("John", "John@gmail.com", "dscdsc", 20, "Toumpa", Types[4], 0, 0),
-    User("John", "John@gmail.com", "dscdsc", 20, "Toumpa", Types[4], 0, 0),
-    User("dsfds", "dscdsc", "dscdsc", 5, "sdfsc", "sdfsc", "sdfsc", "sdfsc"), 
-    User("dsfds", "dscdsc", "dscdsc", 6, "sdfsc", "sdfsc", "sdfsc", "sdfsc"), 
-    User("dsfds", "dscdsc", "dscdsc", 7, "sdfsc", "sdfsc", "sdfsc", "sdfsc")
+    User("John", "John@gmail.com", "dscdc", 20, "Toumpa", Types[4]),
+    User("Helen", "Helen@gmail.com", "fvdfv", 24, "Toumpa", Types[2]),
+    User("Nick", "Nick@gmail.com", "dsccdsc", 21, "Toumpa", Types[2]),
+    User("Ron", "Ron@gmail.com", "dscdcdsc", 20, "Toumpa", Types[1]),
+    User("Paul", "Paul@gmail.com", "scsadwdsc", 19, "Toumpa", Types[6]),
+    User("Emily", "Emily@gmail.com", "sxcdsc", 25, "Toumpa", Types[9]), 
+    User("Rosa", "Rosa@gmail.com", "cvfdsc", 36, "Toumpa", Types[11]), 
+    User("George", "George@gmail.com", "dcsadsc", 18, "Toumpa", Types[6])
+]
+
+Creators = [
+    Creator("John", "John@gmail.com", "dscdsc", 24, "Toumpa", Types[4]),
+    Creator("John", "John@gmail.com", "dscdsc", 28, "Toumpa", Types[2]),
+    Creator("John", "John@gmail.com", "dscdsc", 21, "Toumpa", Types[8]),
+    Creator("John", "John@gmail.com", "dscdsc", 20, "Toumpa", Types[6]),
+    Creator("John", "John@gmail.com", "dscdsc", 21, "Toumpa", Types[4]),
+    Creator("John", "John@gmail.com", "dscdsc", 23, "Toumpa", Types[9]),
+    Creator("John", "John@gmail.com", "dscdsc", 29, "Toumpa", Types[11]),
+    Creator("John", "John@gmail.com", "dscdsc", 18, "Toumpa", Types[9])
 ]
 
 if __name__ == '__main__':
 
-    creator = Creator("John", "John@gmail.com", "dscdsc", 20, "Toumpa", Types[4], 0, 0)
+    creator = Creator("John", "John@gmail.com", "dscdsc", 20, "Toumpa", Types[4])
 
     creator.createEvent("party", "toumpa", "04/11/2020", 5, Types[4], Description)
+    creator.events[0].openEvent()
 
-    creator.invite(Users[0], creator.events[0])
-    Users[0].acceptInvite(0)
+    creator.invite(Users[2], creator.events[0])
+    Users[2].acceptInvite(0)
+    Users[3].joinEvent(creator.events[0])
 
     creator.events[0].showEvent()
-    print(creator.events[0].users)
+
+    for i in range(0, len(creator.events[0].users)):
+        print(creator.events[0].users[i].username)
