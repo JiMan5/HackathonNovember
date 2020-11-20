7fc7be89ca374da5819343199b23ed5639bf2c03
Orestistsira <74668066+Orestistsira@users.noreply.github.com>
2020-11-20 15:28:22 +0200

    Update Event.py

--- Event.py
+++ Event.py
@@ -1,5 +1,4 @@
 import datetime
-from PIL import Image
 
 Types = [
     'programming',
@@ -19,87 +18,101 @@ Types = [
 
 class Event:
     __name = ""
-    #Creator creator = Creator()
     __location = ""
     __date = datetime.datetime.now()
-    __maxPeople = 0
+    __max_people = 0
     __discription = ""
-    __maxWords = 500
-    __typeOfEvent = ""
-    __isOpen = False
+    __max_words = 500
+    __type_of_event = ""
+    __is_open = False
 
-    #print(date.strftime("%x"))
+    #creator = Creator()
+    __users = [] * __max_people
+    __num_of_users = 0
 
-    def __init__(self, name, location, maxPeople, type):
+    def __init__(self, name, location, maxPeople, type, discription):
         self.__name = name
         self.__location = location
-        self.__maxPeople = maxPeople
-        self.__typeOfEvent = type
+        self.__max_people = maxPeople
+        self.__type_of_event = type
+        self.set_discription(discription)
 
-    def setName(self, name):
+    def set_name(self, name):
         self.__name = name
 
-    def getName(self):
+    def get_name(self):
         return self.__name
 
-    def setLocation(self, location):
+    def set_location(self, location):
         self.__location = location
 
-    def getLocation(self):
+    def get_location(self):
         return self.__location
 
-    def setDate(self, day, month, year):
+    def set_date(self, day, month, year):
         self.__date = datetime.datetime(year, month, day)
 
-    def getDate(self):
+    def get_date(self):
         return self.__date
 
-    def setMaxPeople(self, maxPeople):
+    def set_max_people(self, maxPeople):
         self.__maxPeople = maxPeople
 
-    def getMaxPeople(self):
+    def get_max_people(self):
         return self.__maxPeople
 
-    def setDiscription(self, discription):
+    def set_discription(self, discription):
         numOfWords = len(discription.split())
         #print(numOfWords)
-        if numOfWords < self.__maxWords:
+        if numOfWords < self.__max_words:
             self.__discription = discription
         else:
             print('Write less words')
 
-    def getDiscription(self):
+    def get_discription(self):
         return self.__discription
 
-    def getMaxWords(self):
-        return self.__maxWords
+    def get_max_words(self):
+        return self.__max_words
     
-    def setType(self, type):
+    def set_type(self, type):
         if type in Types:
-            self.__typeOfEvent = type
+            self.__type_of_event = type
         else:
             print('Type is not in the list')
 
-    def getType(self):
-        return self.__typeOfEvent
+    def get_type(self):
+        return self.__type_of_event
 
-    def activateEvent(self):
-        self.__isOpen = True
+    def activate_event(self):
+        self.__is_open = True
 
-    def isOpen(self):
-        return self.__isOpen
+    def is_open(self):
+        return self.__is_open
 
-    def showEvent(self):
+    def show_event(self):
         print(self.__name)
         print(self.__location)
-        print(self.__typeOfEvent)
+        print(self.__type_of_event)
         print(self.__date.strftime("%x"))
         print(self.__discription)
-        if self.__isOpen:
+        if self.__is_open:
             print("Event is open")
         else:
             print("Event is carently closed")
         print()
     
+    def add_user(self, user):
+        self.__users[self.__num_of_users] = user
+        self.__num_of_users = self.__num_of_users + 1
 
+    def add_list_of_users(self, users):
+        self.__users = users
+        self.__num_of_users = len(self.__users)
     
+    def get_users(self):
+        self.__users
+
+    def remove_user(self, user_index):
+        self.__users.pop(user_index)
+        self.__num_of_users = self.__num_of_users - 1
