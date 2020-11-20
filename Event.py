fc154b1d4bd76610c28c323f77d08f4450f0554f
Orestistsira <74668066+Orestistsira@users.noreply.github.com>
2020-11-20 16:15:44 +0200

    Update Event.py

--- Event.py
+++ Event.py
@@ -17,117 +17,61 @@ Types = [
 ]
 
 class Event:
-    __name = ""
-    __location = ""
-    __date = datetime.datetime.now()
-    __max_people = 0
-    __discription = ""
-    __max_words = 500
-    __type_of_event = ""
-    __is_open = False
-    __users = [] * __max_people
-    __num_of_users = 0
+    name = ""
+    location = ""
+    date = datetime.datetime.now()
+    max_people = 0
+    discription = ""
+    max_words = 500
+    type_of_event = ""
+    is_open = False
+    users = [] * max_people
     #__creator = Creator()
 
     def __init__(self, name, location, maxPeople, type, discription):
-        self.__name = name
-        self.__location = location
-        self.__max_people = maxPeople
-        self.__type_of_event = type
+        self.name = name
+        self.location = location
+        self.max_people = maxPeople
+        self.type_of_event = type
         self.set_discription(discription)
 
-    def set_name(self, name):
-        self.__name = name
-
-    def get_name(self):
-        return self.__name
-
-    def set_location(self, location):
-        self.__location = location
-
-    def get_location(self):
-        return self.__location
-
     def set_date(self, day, month, year):
-        self.__date = datetime.datetime(year, month, day)
-
-    def get_date(self):
-        return self.__date
-
-    def set_max_people(self, maxPeople):
-        self.__maxPeople = maxPeople
-
-    def get_max_people(self):
-        return self.__maxPeople
+        self.date = datetime.datetime(year, month, day)
 
     def set_discription(self, discription):
         numOfWords = len(discription.split())
         #print(numOfWords)
-        if numOfWords < self.__max_words:
-            self.__discription = discription
+        if numOfWords < self.max_words:
+            self.discription = discription
         else:
             print('Write less words')
 
-    def get_discription(self):
-        return self.__discription
-
-    def get_max_words(self):
-        return self.__max_words
-    
-    def set_type(self, type):
-        if type in Types:
-            self.__type_of_event = type
-        else:
-            print('Type is not in the list')
-
-    def get_type(self):
-        return self.__type_of_event
-
     def open_event(self):
-        self.__is_open = True
+        self.is_open = True
 
-    def close_events(self):
-        self.__is_open = False
+    def close_event(self):
+        self.is_open = False
 
     def is_open(self):
-        return self.__is_open
+        return self.is_open
 
     def show_event(self):
-        print(self.__name)
-        print(self.__location)
-        print('max people: ' + str(self.__max_people))
-        print(self.__type_of_event)
-        print(self.__date.strftime("%x"))
-        print(self.__discription)
-        if self.__is_open:
+        print(self.name)
+        print(self.location)
+        print('max people: ' + str(self.max_people))
+        print(self.type_of_event)
+        print(self.date.strftime("%x"))
+        print(self.discription)
+        if self.is_open:
             print("Event is open")
         else:
             print("Event is currently closed")
         print()
 
     #users
-
-    def is_full(self):
-        return self.__num_of_users == self.__max_people
-    
     def add_user(self, user):
-        if not self.is_full:
-            self.__users[self.__num_of_users] = user
-            self.__num_of_users = self.__num_of_users + 1
-        else:
-            print("Event is full")
-
-    def add_list_of_users(self, users):
-        if self.__num_of_users + len(users) < self.__max_people:
-            self.__users = users
-            self.__num_of_users = len(self.__users)
-        else:
-            print("Event can't add so many people")
-    
-    def get_users(self):
-        self.__users
+        self.users.append(user)
 
     def remove_user(self, user_index):
-        if user_index < self.__num_of_users and user_index > -1:
-            self.__users.pop(user_index)
-            self.__num_of_users = self.__num_of_users - 1
+        if user_index > -1:
+            self.users.pop(user_index)
